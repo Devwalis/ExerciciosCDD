882a849c17e321df1e80bdf9be6d06b979921e99
Walisson oliveira <walissonoliveira1@outlook.com>
2024-09-09 23:51:37 -0300

    Transformar horas em segundos

---
+++ ConverteMinutos.py
@@ -0,0 +1,5 @@
+hora = int(input("Digite a hora atual: "))
+minuto = int(input("Digite o minuto atual: "))
+segundo = int(input("Digite o segundo atual: "))
+horaCorrespondente = 3600 * hora + 60 * minuto + segundo
+print("O tempo correpondente de:  "+ str(hora) + str(minuto) + str(segundo) + "em segundos sera de: " + str(horaCorrespondente))
