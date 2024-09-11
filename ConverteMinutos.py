d83e1468ee225bd4eb3af7d215261b1f3829a5b2
Walisson oliveira <walissonoliveira1@outlook.com>
2024-09-10 22:12:45 -0300

    Converte medida

--- ConverteMinutos.py
+++ ConverteMinutos.py
@@ -2,4 +2,4 @@ hora = int(input("Digite a hora atual: "))
 minuto = int(input("Digite o minuto atual: "))
 segundo = int(input("Digite o segundo atual: "))
 horaCorrespondente = 3600 * hora + 60 * minuto + segundo
-print("O tempo correpondente de:  "+ str(hora) + str(minuto) + str(segundo) + "em segundos sera de: " + str(horaCorrespondente))
+print("O tempo correpondente de:  " + str(hora)+  ":" + str(minuto) + ":" + str(segundo) + ":"+ " em segundos sera de: " + str(horaCorrespondente))
